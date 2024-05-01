17cd77addc7486985fada5bbaa99728de17f1f4f
samy <samy.basso-bert@epitech.eu>
2024-05-01 16:21:18 +0200

    FEAT frontend : add tailwind and shadcnui

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import styled from 'styled-components';
-
+import './globals.css';
 const root = ReactDOM.createRoot(
   document.getElementById('root') ?? document.body
 );
 
-// @INFO: styled-components is installed, you can use it if you want ;)
-const Container = styled.div``;
 
 root.render(
   <StrictMode>
-    <Container>Good luck !</Container>
+    <div className='text-red-500'>hello world!</div> 
   </StrictMode>
 );
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import { scripts } from './package.json';
+import path from "path"
 
 export default defineConfig({
   root: __dirname,
@@ -24,6 +25,12 @@ export default defineConfig({
     nxViteTsPaths(),
     checker({ typescript: true, eslint: { lintCommand: scripts.lint } }),
   ],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   build: {
     outDir: '../../dist/apps/frontend',
