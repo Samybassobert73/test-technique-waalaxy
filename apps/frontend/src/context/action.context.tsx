4955ff99b076118c1df2a44a0cbd256a0b05a2d5
samy <samy.basso-bert@epitech.eu>
2024-05-01 20:22:40 +0200

    FEAT frontend : add action context

---
+++ apps/frontend/src/context/action.context.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, useContext, useState } from 'react';
+import ActionI from '../interfaces/action.interface';
+
+export type ActionContextType = {
+    actions: ActionI[];
+    setActions: (actions:ActionI[]) => void;
+    addAction: (newAction:ActionI) => void;
+    removeAction: (id:string) => void;
+};
+
+  
+export const ActionContext = createContext<ActionContextType|undefined>(undefined);
+
+export const useAction = () => useContext(ActionContext);
+
+
+export const ActionProvider = ({ children }: { children: React.ReactNode }) => {
+    const [actions, setActions] = useState<ActionI[]>([]);    
+
+    const addAction = (newAction:ActionI): void => {
+        setActions(prevActions => [...prevActions, newAction])
+    }
+
+    const removeAction = (id:string): void => {
+        setActions(prevActions => prevActions.filter(action => action._id !== id))
+    }
+    return (
+        <ActionContext.Provider value={{actions, setActions, addAction, removeAction}}>
+            {children}
+        </ActionContext.Provider>
+    );
+};
